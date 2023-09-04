e0711e45f50a81f4eef59d8737e700f3976a0efe
ldimov09 <luboslavdimov@gmail.com>
2023-09-04 15:00:19 +0300

    Set up php server

--- backend/config/routes.js
+++
@@ -1,5 +0,0 @@
-const homeContorller = require('../controllers/homeController.js')
-
-module.exports = (app) => {
-    app.use('/', homeContorller);
-};
--- backend/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-const homeController = require('express').Router();
-
-homeController.get('/', function (req, res) {
-    res.send(JSON.stringify({Hello: "World"}));
-});
-
-module.exports = homeController;
